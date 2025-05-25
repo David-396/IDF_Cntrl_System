8fd3952a92fbcd2d037f23b2cb3e3b1e9df25153
David-396 <david.eini12@gmail.com>
2025-05-25 10:10:04 +0300

    add combat and organize with directories

---
+++ IDF_Cntrl_System/OBJECTS/Combat/ACombat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Cntrl_System.OBJECTS.Combat
+{
+    internal class ACombat
+    {
+        protected List<string> BombType { get; }
+        protected List<string> EfficientVS { get; }
+        protected string ActivatedBy { get; }
+
+        protected ACombat(List<string> bombType, List<string> efficientVS, string activatedBy)
+        {
+            BombType = bombType;
+            EfficientVS = efficientVS;
+            ActivatedBy = activatedBy;
+        }
+    }
+}
---
+++ IDF_Cntrl_System/OBJECTS/Combat/IDF_combats/AIR/F16_AirCraft.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Cntrl_System.OBJECTS.Combat.IDF_combats.AIR
+{
+    internal class F16_AirCraft : ACombat
+    {
+        public F16_AirCraft(List<string> bombType, List<string> efficientVS, string activatedBy) : base(bombType, efficientVS, activatedBy) { }
+    }
+}
